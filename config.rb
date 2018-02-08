fe8f32fbea28f158d4a21c8c673749ebdb214cf6
Vinchenso <chensok@gmail.com>
2018-02-08 18:08:55 +0200

    topbar styled

--- config.rb
+++ config.rb
@@ -31,7 +31,7 @@ set :fonts_dir, './fonts'
 set :images_dir, './images'
 set :js_dir, './javascripts'
 
-page '/', layout: false
+# page '/', layout: false
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
