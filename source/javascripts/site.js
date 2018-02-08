cf36895d4a1fb97b1f432061074d4c15162f02c8
Vinchenso <chensok@gmail.com>
2018-02-08 09:12:37 +0200

    config working before fonts added

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,3 +1,3 @@
-import 'core';
-import 'thesaas.min.js';
-import 'script';
+import './core.min.js';
+import './thesaas.min.js';
+import './script';
--- webpack.config.js
+++ webpack.config.js
@@ -37,17 +37,6 @@ module.exports = {
           ]
         })
       },
-      {
-         test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-         use: [{
-           loader: 'file-loader',
-           options: {
-             name: '[name].[ext]',
-             outputPath: 'fonts/',    // where the fonts will go
-             publicPath: '../'       // override the default path
-           }
-         }]
-      }
     ],//end rules
   },
   output: {
